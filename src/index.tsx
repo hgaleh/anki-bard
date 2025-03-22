e3333ef50b4fa461512bbc1e5a8581f3478b1062
Dario <dariogaleh@outlook.com>
2025-03-22 23:16:13 +0100

    sanitise the content of the input text

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,25 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import DOMPurify from 'dompurify';
 
 class MyCustomElement extends HTMLElement {
   connectedCallback() {
     const mountPoint = document.createElement('div');
     this.attachShadow({ mode: 'open' }).appendChild(mountPoint);
 
-    const answer = this.getAttribute('answer') || '';
+    const answer = this.innerHTML || '';
+
+    const sanitizedAnswer = removeHtmlTags(answer)
+      .replace(/&[^;\s]+;/g, '')
+      .replace(/[\u200B-\u200D\uFEFF]/g, '')
+      .replace(/[\n\r\t]/g, ' ')
+      .replace(/\s\s+/g, ' ')
+      .trim(); ;
     const state = this.getAttribute('state') === 'front' ? 'front' : 'back';
 
     const root = ReactDOM.createRoot(mountPoint);
-    root.render(<App answer={answer} state={state}></App>);
+    root.render(<App answer={sanitizedAnswer} state={state}></App>);
   }
 
   disconnectedCallback() {
@@ -21,3 +29,8 @@ class MyCustomElement extends HTMLElement {
 customElements.define('anki-bard', MyCustomElement);
 
 reportWebVitals();
+
+function removeHtmlTags(input: string) {
+  // Remove all tags by allowing none
+  return DOMPurify.sanitize(input, { ALLOWED_TAGS: [], ALLOWED_ATTR: [] });
+};
